7ebb577aab0b1ebb6e3b90c23607a75cd94be8bd
HebaNAS <HebaNAS@users.noreply.github.com>
2018-02-07 10:26:36 +0400

    Added comments to command.js

--- command.js
+++ command.js
@@ -1,7 +1,7 @@
-// Find _ids for matching locations based on some criteria (eg. "abbrev")
+// Find _ids for matching routes where the src airport is DXB
 var matches = db.routes.find({src_ap: "DXB"}, {"dst_apid": 1, _id: 0});
 
-// Transform matching _id values into an array
+// Store matching _id values into an array of ObjectIDs
 var dst = [];
 matches.forEach(function(match) {
   if (match.dst_apid != null) {
@@ -9,10 +9,12 @@ matches.forEach(function(match) {
   }
 });
 
-// Look for widgets matching the location criteria
+// Iterate through the array of ObjectIDs holding matching values of the previous query
+// and use each id to find the matching airport, then get the name of that airport
 var results = [];
 dst.forEach(function(one) {
   results.push(db.airports.find({"_id": one}, {"name": 1, "_id": 0}).toArray());
 });
 
+// Print results as json
 printjson(results);
